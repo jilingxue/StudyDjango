adc4edb1bb28f4c368a43705ea72dfec773bc850
xuejiling <1633882338@qq.com>
2019-01-17 00:59:31 +0800

    首页和登录页面

--- MxOnline/MxOnline/urls.py
+++ MxOnline/MxOnline/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 import xadmin
+from users.views import user_login
 
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('login/', user_login, name='login'),
 ]
